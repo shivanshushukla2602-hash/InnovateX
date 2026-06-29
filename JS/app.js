184a4f4147d5e95b3d149f2e99230ca40048042c
shivanshu19 <shivanshushukla2602@gmail.com>
2026-06-30 00:26:11 +0530

    Rename Flipkart to Amazon and update portfolio links

---
+++ JS/app.js
@@ -0,0 +1,100 @@
+let themeBtn = document.querySelector("#themeBtn");
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+}
+/* why this condition? [localStorage.getItem("theme") === "dark"]
+To check the previous state of the theme and if it was dark then we will add the dark class to body so that when user refreshes the page the theme remains same as before
+*/
+
+themeBtn.addEventListener("click" , function(){
+    document.body.classList.toggle("dark");
+    /*
+    Why this condition? [document.body.classList.toggle("dark")]
+    agar dark class hai body me to use hata dega aur agar nahi to use add kar dega
+    */
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme" , "dark");
+    }
+    else{
+        localStorage.setItem("theme" , "light");
+    }
+})
+
+
+renderMembers();
+renderProjects();
+
+const contactForm = document.querySelector("#contactForm");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
+const nameMsg = document.querySelector("#nameMsg");
+const emailMsg = document.querySelector("#emailMsg");
+const textareaMsg = document.querySelector("#textareaMsg");
+const emailHelp = document.querySelector("#emailHelp");
+
+const submitBtn = document.querySelector("#submitBtn");
+
+
+
+submitBtn.addEventListener("click" , function(event){
+
+    event.preventDefault();
+
+    // Flag ---> check karne ke liye agar ek bhi invalid nikla to hum input field ka data erase nahi karenge
+    let isValid = true;
+
+    let name = nameInput.value.trim();
+    let email = emailInput.value.trim();
+    let message = messageInput.value.trim();
+
+    // Reset old messages
+    nameMsg.textContent = "";
+    emailMsg.textContent = "";
+    textareaMsg.textContent = "";
+
+    // Name Validation
+    if(name === ""){
+        nameMsg.textContent = "Name is required";
+        nameMsg.style.color = "red";
+        isValid = false;
+    }
+
+    // Message Validation
+    if(message === ""){
+        textareaMsg.textContent = "Message is required";
+        textareaMsg.style.color = "red";
+        isValid = false;
+    }
+
+    // Email Validation
+    if(email === ""){
+        emailMsg.textContent = "Email is required";
+        emailMsg.style.color = "red";
+        emailHelp.style.display = "none";
+        isValid = false;
+    }
+    else if(!email.includes("@") || !email.includes(".")){
+        emailMsg.textContent = "Enter a valid email";
+        emailMsg.style.color = "red";
+        emailHelp.style.display = "none";
+        isValid = false;
+    }
+    else{
+        emailHelp.style.display = "none";
+    }
+
+    // Clear fields only if everything is valid
+    if(isValid){
+        nameInput.value = "";
+        emailInput.value = "";
+        messageInput.value = "";
+
+        alert("Form Submitted Successfully");
+    }
+
+});
+
+
---
+++ JS/state.js
@@ -0,0 +1,72 @@
+const members = [
+    {
+        photo : "../assets/images/shivanshu.png",
+        name : "Shivanshu Shukla" , 
+        role : "Backend & DevOps" , 
+        Contributions : ["Portfolio"]  ,
+        github : "https://github.com/shivanshushukla2602-hash"
+    } , 
+    {
+        photo : "../assets/images/hasini.png",
+        name : "Batchu Lakshmi Hasini" , 
+        role : "UI/UX Designer" , 
+        github : "https://github.com/LakshmiHasini"
+    } , 
+    {
+        photo : "../assets/images/saketh.png",
+        name : "Pingili Saketh Reddy" , 
+        role : "Backend Developer" , 
+        github : "https://github.com/sakethreddypingili"
+    } , 
+    {
+        photo : "../assets/images/satyam.png",
+        name : "Satyam Kumar Jha" , 
+        role : "Frontend Developer" , 
+        github : "https://github.com/satyamkjha001-cpu"
+    } , 
+    {
+        photo : "../assets/images/mahendra.png",
+        name : "Mahendra Yadav" , 
+        role : "Frontend Developer" , 
+        github : "https://github.com/my9941652-create"
+    } 
+]
+
+const projects = [
+{
+    title:"Quiz App",
+    description:"Interactive MCQ Quiz Application",
+    tech:"HTML, CSS, JavaScript",
+    github:"#"
+},
+{
+    title:"Expense Tracker",
+    description:"Track Income and Expenses",
+    tech:["HTML, CSS, JavaScript"],
+    github:"#"
+},
+{
+    title:"News Feed",
+    description:"Latest News Application",
+    tech:["API, HTML, CSS, JS"],
+    github:"#"
+},
+{
+    title:"GitHub Explorer",
+    description:"Search GitHub Profiles",
+    tech:["GitHub API"],
+    github:"#"
+},
+{
+    title:"Kanban Board",
+    description:"Task Management Board",
+    tech:["JavaScript"],
+    github:"../Kanban Board/HTML/index.html"
+},
+{
+    title:"Portfolio",
+    description:"Team Portfolio Website",
+    tech:["HTML, CSS, JS"],
+    github:"#"
+}
+];
